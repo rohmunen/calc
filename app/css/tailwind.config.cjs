ca83f3ef847b17284fd4de5c6e6fc1b6bd8b29b9
Daniil Salyn <salyndaniil@gmail.com>
2023-03-09 14:29:41 +0300

    feat: initial setup

---
+++ app/css/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./app/src/index.html', './app/src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/style.css';
+
+function App() {
+	return (
+		<div>
+			<div className="h-screen w-full">App</div>
+		</div>
+	);
+}
+
+export default App;
